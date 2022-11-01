237a81d627de39c1a7c1d4827eef999029c73f85
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2022-11-01 23:49:05 +0200

    encode species column

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ from matplotlib import pyplot as plt, pyplot
 
 data = pd.read_csv('penguins.csv')
 
+# find important columns name which contain  numeric values
 numbers_cols = data.select_dtypes(include= np.number).columns.to_list()
+
+# find important columns name which contain nun numeric values & convert it's type to string
+non_integer_cols = data.select_dtypes(include=['object']).columns.to_list()
+data[non_integer_cols] = data[non_integer_cols].astype('string')
+
+# encode species column
+label_encoders = []
+label_encoder = preprocessing.LabelEncoder()
+data['species'] = label_encoder.fit_transform(data['species'])
+label_encoders.append(label_encoder)
 
 
